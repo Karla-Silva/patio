91303d037634a6ead15ce6acc494dc114df978a3
Karla-Silva <mkarla@outlook.com>
2023-03-06 19:13:43 -0300

    Initial commit

---
+++ src/main/java/com/estacionamento/patio/PatioApplication.java
@@ -0,0 +1,13 @@
+package com.estacionamento.patio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estacionamento/patio/PatioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estacionamento.patio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
