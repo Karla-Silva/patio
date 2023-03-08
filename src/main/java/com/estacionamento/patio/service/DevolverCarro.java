6cdace5e7cffe876637e9b79c525904cf3bb5d21
Karla-Silva <mkarla@outlook.com>
2023-03-07 23:16:58 -0300

    rotas REST

--- src/main/java/com/estacionamento/patio/service/DevolverCarro.java
+++ src/main/java/com/estacionamento/patio/service/DevolverCarro.java
@@ -1,7 +1,11 @@
 package com.estacionamento.patio.service;
 
 import com.estacionamento.patio.repositories.VagaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class DevolverCarro {
     private VagaRepository vagaRepository;
 
